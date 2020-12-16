591318b74447eaff1d1234dcc1efcd382bf20acc
leroli <leroli@tencent.com>
2020-12-16 20:00:26 +0800

    feat: 增加mysql数据库连接配置

--- config/config.default.js
+++ config/config.default.js
@@ -1,30 +1,55 @@
 /* eslint valid-jsdoc: "off" */
-
-'use strict'
-
+'use strict';
 /**
- * @param {Egg.EggAppInfo} appInfo app info
+ \* @param *{Egg.EggAppInfo}* *appInfo* app info
  */
 module.exports = (appInfo) => {
   /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
+   \* built-in config
+   \* @type *{Egg.EggAppConfig}*
    **/
-  const config = (exports = {})
-
+  const config = (exports = {});
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1606557579995_1588'
-
+  config.keys = appInfo.name + '_1606557579995_1588';
   // add your middleware config here
-  config.middleware = []
+  config.middleware = [];
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
+  config.mysql = {
+    // database configuration
+
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: '',
+      // database
+      database: 'blog',
+    },
+
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
 
   // add your user config here
+
   const userConfig = {
     // myAppName: 'egg',
-  }
+  };
 
   return {
     ...config,
     ...userConfig,
-  }
-}
+  };
+};
